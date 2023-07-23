524353e6d124c3dda567142bdd5f0d5c61a05383
Madaliev Samandar <samandarmadaliev@yandex.com>
2023-07-23 17:22:43 +0500

    Updated index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
   require_once "./core/bootstrap.php";
-  // $controller = new Controller(file_get_contents("php://input"));
-  $controller = new Controller('{"update_id":651233947,"message":{"message_id":594,"from":{"id":1124955251,"is_bot":false,"first_name":"Samandar","username":"LuckySaman","language_code":"en"},"chat":{"id":1124955251,"first_name":"Samandar","username":"LuckySaman","type":"private"},"date":1689614689,"text":"/start"}}');
+  $controller = new Controller(file_get_contents("php://input"));
